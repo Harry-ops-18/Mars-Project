c6c43bb97a02e122be3d1d080532dbe859e77717
Harry-ops-18 <anaksholeh8899@gmail.com>
2023-05-20 18:24:19 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
-if [! .d $VIRTUALENV]; then
+if [ ! -d $VIRTUALENV ]; then
     python3 -m venv $VIRTUALENV
 fi
 
-if [! -f $VIRTUALENV/bin/pip]; then 
+if [ ! -f $VIRTUALENV/bin/pip ]; then 
     curl --silent --show-eror --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-$VIRTUALENV/bin/python3 app.python3
+$VIRTUALENV/bin/python3 app.py
 Footer
